c773bf828ed4ec29aba44a6f5ad52b066ef41988
Buwaneka Sudheera Thisarasinghe <you@example.comsudheerabuwaneka@gmail.com>
2020-05-17 14:39:46 +0530

    Products database CRUD

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb+srv://node-shop:' + process.env.MONGO_ATLAS_PW + '@node-rest-shop-06dca.mongodb.net/test?retryWrites=true&w=majority', {
+    // useMongoClient: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.Promise = global.Promise;
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,7 +26,7 @@ app.use((req, res, next) => {
         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     );
 
-    if(req.method === 'OPTIONS'){
+    if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
         return res.status(200).json({});
     }
